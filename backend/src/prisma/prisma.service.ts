70a4a3ae8a6e562bc3c2191bfa561f31a04608fb
Walber Vaz <wvs.walber@gmail.com>
2024-12-31 19:14:26 -0300

    feat: enhance PrismaService constructor to include logging options

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -6,6 +6,12 @@ export class PrismaService
     extends PrismaClient
     implements OnModuleInit, OnModuleDestroy
 {
+    constructor() {
+        super({
+            log: ['query', 'info', 'warn', 'error'],
+        });
+    }
+
     async onModuleInit() {
         await this.$connect();
     }
