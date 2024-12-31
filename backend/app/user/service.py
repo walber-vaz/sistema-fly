a02bd1d01e4f8b6decca8a22a191b34a99761f6c
Walber Vaz <wvs.walber@gmail.com>
2024-12-31 11:02:53 -0300

    feat: enhance user service with client management and logging

--- backend/app/user/service.py
+++ backend/app/user/service.py
@@ -1,3 +1,4 @@
+import logging
 from http import HTTPStatus
 from uuid import UUID
 
@@ -8,15 +9,24 @@ from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.security.hash import get_password_hash
+from app.user.dependencies import return_data_client_user, validate_email_phone
 from app.user.exceptions import (
     EmailAlreadyRegisteredException,
     ErrorCreateUserException,
     PhoneNumberAlreadyRegisteredException,
-    UserNotFoundException,
 )
-from app.user.model import User
-from app.user.schemas import UserCreate, UserRead, UserResponse, UserUpdate
+from app.user.model import Client, User
+from app.user.schemas import (
+    ClientGetData,
+    ClientRead,
+    ClientUpdate,
+    UserCreate,
+    UserRead,
+    UserResponse,
+)
+from app.utils import RoleEnum
 
+logger = logging.getLogger(__name__)
 h.activate('pt_BR')
 
 
@@ -45,9 +55,18 @@ async def store(user: UserCreate, session: AsyncSession):
             phone_number=user.phone_number,
             password=pwd_hash,
             role=user.role,
+            is_active=user.is_active,
         )
 
         session.add(result)
+        session.flush()
+
+        client = Client(
+            user_id=result.id,
+            company_name=user.company_name,
+        )
+
+        session.add(client)
         await session.commit()
 
         return UserResponse(
@@ -58,6 +77,9 @@ async def store(user: UserCreate, session: AsyncSession):
         raise ErrorCreateUserException(
             'Erro ao salvar usuário',
         )
+    except HTTPException as e:
+        logger.error(e)
+        raise e
     except Exception:
         raise HTTPException(
             status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
@@ -66,16 +88,11 @@ async def store(user: UserCreate, session: AsyncSession):
 
 
 async def get_user_by_id(id: UUID | None, session: AsyncSession):
-    stmt = select(User).where(User.id == id)
-    result = await session.scalar(stmt)
-
-    if not result:
-        raise UserNotFoundException('Usuário não encontrado')
+    _, result = await return_data_client_user(id, session)
 
     humanize_created_at = h.naturaltime(result.created_at)
     humanize_updated_at = h.naturaltime(result.updated_at)
-    response = UserRead(
-        id=result.id,
+    user = UserRead(
         email=result.email,
         first_name=result.first_name,
         surname=result.surname,
@@ -84,41 +101,42 @@ async def get_user_by_id(id: UUID | None, session: AsyncSession):
         updated_at=humanize_updated_at,
     )
 
+    client = ClientRead(
+        id=result.client.id,
+        company_name=result.client.company_name,
+        created_at=humanize_created_at,
+        updated_at=humanize_updated_at,
+    )
+
+    response = ClientGetData(
+        id=client.id,
+        company_name=client.company_name,
+        email=user.email,
+        first_name=user.first_name,
+        surname=user.surname,
+        phone_number=user.phone_number,
+        created_at=client.created_at,
+        updated_at=client.updated_at,
+    )
+
     return response
 
 
-async def update(id: UUID, user: UserUpdate, session: AsyncSession):
+async def update(id: UUID, client: ClientUpdate, session: AsyncSession):
     try:
-        stmt = select(User).where(User.id == id)
-        user_to_update = await session.scalar(stmt)
+        result_client, result = await return_data_client_user(id, session)
 
-        if not user_to_update:
-            raise UserNotFoundException('Usuário não encontrado')
+        await validate_email_phone(client, session)
 
-        if user.email:
-            stmt = select(User).where(User.email == user.email)
-            existing_email_user = await session.scalar(stmt)
-
-            if existing_email_user:
-                if existing_email_user.email == user.email:
-                    raise EmailAlreadyRegisteredException(
-                        'Email já cadastrado',
-                    )
-
-        if user.phone_number:
-            stmt = select(User).where(User.phone_number == user.phone_number)
-            existing_phone_user = await session.scalar(stmt)
-
-            if existing_phone_user:
-                if existing_phone_user.phone_number == user.phone_number:
-                    raise PhoneNumberAlreadyRegisteredException(
-                        'Telefone já cadastrado',
-                    )
-
-        for key, value in user.model_dump(exclude_none=True).items():
+        for key, value in client.model_dump(exclude_none=True).items():
             if key == 'password':
                 value = get_password_hash(value)
-            setattr(user_to_update, key, value)
+            if key == 'company_name':
+                setattr(result_client, key, value)
+            if key == 'role':
+                if value not in RoleEnum:
+                    raise ValueError('Role não existe')
+            setattr(result, key, value)
 
         await session.commit()
 
