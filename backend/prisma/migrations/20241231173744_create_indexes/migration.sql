14f9a373c854c8f3219e8979964fd9777169b509
Walber Vaz <wvs.walber@gmail.com>
2025-01-01 10:23:35 -0300

    chore: remove obsolete migration files for database schema updates

--- backend/prisma/migrations/20241231173744_create_indexes/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateIndex
-CREATE INDEX "tb_users_lastLogin_idx" ON "tb_users"("lastLogin");
-
--- CreateIndex
-CREATE INDEX "tb_users_failedLoginAttempts_idx" ON "tb_users"("failedLoginAttempts");
-
--- CreateIndex
-CREATE INDEX "tb_users_permissions_granted_idx" ON "tb_users_permissions"("granted");
