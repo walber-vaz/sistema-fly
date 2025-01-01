4d42a14a5b93892535a27fc67a3357bd1fc03daa
Walber Vaz <wvs.walber@gmail.com>
2025-01-01 10:23:48 -0300

    feat: define User entity with properties and add UserType enum for user roles

--- backend/src/user/entities/user.entity.ts
+++ backend/src/user/entities/user.entity.ts
@@ -1,1 +1,13 @@
-export class User {}
+import { UserType } from '../enums';
+
+export class User {
+    id?: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    addressId?: string;
+    type: UserType = UserType.CUSTOMER;
+    isActive: boolean = true;
+    phone: string;
+}
